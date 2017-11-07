9d093af3f4e1e828ad2d4228f935bb85170c8bd6
theWildSushii <sushiiito123@gmail.com>
2017-11-07 09:18:25 -0700

    Worked on game view

--- Hare Editor/Controls/Game.cs
+++ Hare Editor/Controls/Game.cs
@@ -1,6 +1,21 @@
-﻿namespace HareEditor {
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace HareEditor {
 
     public class Game : DBPanel {
+
+        private HareGLControl glcontrol;
+
+        public Game() {
+            try {
+                glcontrol = new HareGLControl();
+                glcontrol.Dock = System.Windows.Forms.DockStyle.Fill;
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        }
 
     }
 
